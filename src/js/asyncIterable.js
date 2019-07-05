71d8940b60a3d55f727a64fa5284266c7692959b
GuOshiro <gustavoaoshiro@gmail.com>
2019-07-05 08:29:47 -0300

    Adicionando exemplo de iterators

--- src/js/asyncIterable.js
+++ src/js/asyncIterable.js
@@ -17,7 +17,7 @@ function AsyncIterable(...args) {
 
 async function iteracaoSobreAsyncIterable(text) {
     let newText = text;
-    for await (let val of AsyncIterable("hamburguer", "pizza", "baz")) {
+    for await (let val of AsyncIterable('hamburguer', 'pizza', 'danoninho')) {
         newText += `${val}, `;
     }
 
@@ -25,7 +25,36 @@ async function iteracaoSobreAsyncIterable(text) {
 }
 
 async function gerarFrase(text) {
-    let newText = await iteracaoSobreAsyncIterable(text);
-    let myDivElement = document.getElementById('aDiv');
-    myDivElement.innerHTML = '<p>' + newText + '</p>';
+    let listaAlimento = await iteracaoSobreAsyncIterable(text);
+    let alimentosHtml = document.getElementById('alimentos');
+    alimentosHtml.innerHTML = '<p>' + listaAlimento + '</p>';
 }
+
+gerarFrase('Os melhores alimentos são ')
+
+
+function asyncIteratorAlimentos() {
+    const listaAlimentos = [{ alimento: 'pizza', caloria: 2000 }, { alimento: 'Hamburguer', caloria: 1000 }, { alimento: 'danoninho', caloria: 5000 }];
+    return {
+        next: function () {
+            if (listaAlimentos.length) {
+                return Promise.resolve({
+                    value: listaAlimentos.shift(),
+                    done: false
+                });
+            } else {
+                return Promise.resolve({
+                    done: true
+                });
+            }
+        }
+    };
+}
+
+var iterator = asyncIteratorAlimentos();
+
+(async function () {
+    await iterator.next().then(console.log);
+    await iterator.next().then(console.log);
+    await iterator.next().then(console.log);
+})();
