6377516ee31d0f550794ee34f51766898d520f0a
GuOshiro <gustavoaoshiro@gmail.com>
2019-07-09 18:20:38 -0300

    Correção do asyncIterable

--- src/js/asyncIterable.js
+++ src/js/asyncIterable.js
@@ -1,60 +1,71 @@
-function AsyncIterable(...args) {
-    let index = 0
-    let asyncIterable = {
-        [Symbol.asyncIterator]() {
-            return this;
-        },
-        next() {
-            if (index < args.length) {
-                return Promise.resolve({ value: args[index++], done: false })
-            } else {
-                return Promise.resolve({ done: true })
-            }
+//iterable com async await
+function tabelaAlimentos() {
+    const tabelas = {
+        alimento: {
+            1: { nome: 'Maça' },
+            2: { nome: 'Pizza' },
+            3: { nome: 'Feijoada' },
+            4: { nome: 'Hamburguer' },
         }
-    };
-    return asyncIterable;
-}
-
-async function iteracaoSobreAsyncIterable(text) {
-    let newText = text;
-    for await (let val of AsyncIterable('hamburguer', 'pizza', 'danoninho')) {
-        newText += `${val}, `;
     }
-
-    return newText;
-}
-
-async function gerarFrase(text) {
-    let listaAlimento = await iteracaoSobreAsyncIterable(text);
-    let alimentosHtml = document.getElementById('alimentos');
-    alimentosHtml.innerHTML = '<p>' + listaAlimento + '</p>';
-}
-
-gerarFrase('Os melhores alimentos são ')
-
-
-function asyncIteratorAlimentos() {
-    const listaAlimentos = [{ alimento: 'pizza', caloria: 2000 }, { alimento: 'Hamburguer', caloria: 1000 }, { alimento: 'danoninho', caloria: 5000 }];
     return {
-        next: function () {
-            if (listaAlimentos.length) {
-                return Promise.resolve({
-                    value: listaAlimentos.shift(),
+        getSync: (nome, id) => tabelas[nome][id],
+        getAsync: (nome, id) => delay(50).then(() => tabelas[nome][id]),
+    }
+}
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+const tabela = tabelaAlimentos()
+
+//iterable normal
+const asyncIteratorAlimentos = {
+    [Symbol.iterator]: () => {
+        let i = 0;
+        return {
+            next: () => {
+                i++;
+                const alimento = tabela.getSync('alimento', i)
+                if (alimento) {
+                    return { value: alimento, done: false };
+                }
+                return { done: true }
+            }
+        };
+    }
+}
+
+for (const alimento of asyncIteratorAlimentos) {
+    console.log(alimento)
+}
+
+
+//iterable async
+const alimentosAsync = {
+    [Symbol.iterator]: function () {
+        let i = 0;
+        return {
+            next: async function () {
+                i++;
+                const alimento = await tabela.getAsync('alimento', i)
+                if (!alimento) {
+                    return { done: true }
+                }
+                return {
+                    value: alimento,
                     done: false
-                });
-            } else {
-                return Promise.resolve({
-                    done: true
-                });
+                }
             }
         }
-    };
-}
+    }
+};
 
-var iterator = asyncIteratorAlimentos();
 
 (async function () {
-    await iterator.next().then(console.log);
-    await iterator.next().then(console.log);
-    await iterator.next().then(console.log);
-})();
+    const iterator = alimentosAsync[Symbol.iterator]()
+    console.log((await iterator.next()).value)
+    console.log((await iterator.next()).value)
+    console.log((await iterator.next()).value)
+    console.log((await iterator.next()).value)
+})()
